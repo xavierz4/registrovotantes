2e56754b485f0c1650ec94fd05cb47bd208a5337
javier <jjzoto@gmail.com>
2019-04-18 15:48:49 -0500

    master

---
+++ src/mutations/index.js
@@ -0,0 +1,11 @@
+import gql from 'graphql-tag';
+
+export const NUEVO_CLIENTE = gql`
+mutation CrearCliente($input:ClienteInput){
+    crearCliente(input:$input){
+      id
+      nombre
+      apellido
+    }
+  }
+  `;
