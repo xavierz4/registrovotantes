ed9276f6beb18e04bf05340fd6d5082bb824bedc
javier <jjzoto@gmail.com>
2019-04-19 17:43:02 -0500

    elector act

--- src/componentes/NuevoElector.js
+++ src/componentes/NuevoElector.js
@@ -1,10 +1,11 @@
 import React, { Component, Fragment } from 'react';
+
 import {NUEVO_ELECTOR} from '../mutations';
 import {Mutation} from 'react-apollo';
 
+
 class NuevoElector extends Component {
     state = { 
-
         elector:{
             nombre:'',
             apellido:'',
@@ -19,144 +20,127 @@ class NuevoElector extends Component {
         return (
 
             <Fragment>
-                    <h2 className="text-center mt-4">Nuevo Elector</h2>   
+                    <h2 className="text-center">Nuevo Elector</h2>   
                     <div className="row justify-content-center">
-                    <Mutation mutation={NUEVO_ELECTOR}>
-                    {crearElector =>(
-                        <form 
-                            className="col-md-8 m-3"
-                           
-                            onSubmit={e =>{
-                                e.preventDefault();
-                                const {nombre, apellido, cedula, edad, genero, email } = this.state.elector;
+                     <Mutation mutation={NUEVO_ELECTOR}>
+                        {crearElector =>(
+                            <form
+                                className="col-md-8 m-3"
+                                onSubmit={e =>{
+                                    e.preventDefault();
+                                    const {nombre, apellido, cedula, edad, genero, email } = this.state.cliente;
+                                    
+                                    const input = {
+                                        nombre,
+                                        apellido,
+                                        cedula: Number(cedula),
+                                        edad: Number(edad),
+                                        genero,
+                                        email
+                                    };
+                                    crearElector({
+                                        variables: {input}
+                                    })
+                                }}
+                            
+                            >
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label>Nombre</label>
+                                            <input 
+                                            type="text"
+                                            class="form-control" 
+                                            placeholder="nombre"
+                                            onChange={e =>{
+                                                this.setState({
+                                                    elector:{
+                                                        ...this.state.elector,
+                                                        nombre: e.target.value
+                                                    }
+                                                })
+                                            }}
+                                        />
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label>Apellido</label>
+                                        <input 
+                                            type="text" 
+                                            class="form-control" 
+                                            placeholder="Apellido"
+                                            onChange={e =>{
+                                                this.setState({
+                                                    elector:{
+                                                        ...this.state.elector,
+                                                        apellido: e.target.value
+                                                    }
+                                                })
+                                            }}
+                                        
+                                        />
+                                        
+                                    </div>
+                                </div>
                                 
-                                const input = {
-                                   nombre,
-                                   apellido,
-                                   cedula: Number(cedula),
-                                   edad: Number(edad),
-                                   genero,
-                                   email
-                                  
-                                };
-                                console.log(input)
-                            }}
-                        
-                        >
-                            <div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <label>Nombre</label>
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label>Cedula</label>
+                                        <input 
+                                            type="text" 
+                                            class="form-control" 
+                                            placeholder="Empresa" 
+                                            onChange={e =>{
+                                                this.setState({
+                                                    elector:{
+                                                        ...this.state.elector,
+                                                        cedula: e.target.value
+                                                    }
+                                                })
+                                            }}
+                                            />
+                                    </div>
+                                
+                                    <div class="form-group col-md-6">
+                                        <label>Email</label>
+                                        <input 
+                                            type="email"
+                                            class="form-control"
+                                            placeholder="Email"
+                                            onChange={e =>{
+                                                this.setState({
+                                                    elector:{
+                                                        ...this.state.elector,
+                                                        email: e.target.value
+                                                    }
+                                                })
+                                            }} 
+                                        />
+                                    </div>
+                                </div>
+
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label>Edad</label>
                                         <input
-                                        type="text"
-                                        class="form-control" 
-                                        placeholder="nombre"
-                                        onChange={e =>{
-                                            this.setState({
-                                                elector:{
-                                                    ...this.state.elector,
-                                                    nombre: e.target.value
-                                                }
-                                            })
-                                        }}
-                                      />
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label>Apellidos</label>
-                                    <input
-                                        type="text" 
-                                        class="form-control"
-                                        placeholder="Apellido"
-                                        onChange={e =>{
-                                            this.setState({
-                                                elector:{
-                                                    ...this.state.elector,
-                                                    apellido: e.target.value
-                                                }
-                                            })
-                                        }}
-                                      />
-                                </div>
-                            </div>
+                                            type="text"
+                                            class="form-control" 
+                                            placeholder="Edad"
+                                            onChange={e =>{
+                                                this.setState({
+                                                    elector:{
+                                                        ...this.state.elector,
+                                                        edad: e.target.value
+                                                    }
+                                                })
+                                            }}
+                                        />
+                                    </div>
+                                
 
-                            <div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <label>Cedula</label>
-                                    <input
-                                     type="text"
-                                     class="form-control"
-                                     placeholder="nombre"
-                                     onChange={e =>{
-                                        this.setState({
-                                            elector:{
-                                                ...this.state.elector,
-                                                cedula: e.target.value
-                                            }
-                                        })
-                                    }}
-                                     
-                                     />
-
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label>Edad</label>
-                                    <input 
-                                        type="text" 
-                                        class="form-control" 
-                                        placeholder="Fecha Nacimiento"
-                                        onChange={e =>{
-                                            this.setState({
-                                                elector:{
-                                                    ...this.state.elector,
-                                                    edad: e.target.value
-                                                }
-                                            })
-                                        }}
-                                    />
-                                </div>
-                            </div>
-                            
-                            <div class="form-row">
-                                <div class="form-group col-md-6">
-                                    <label>Género</label>
-                                    <select  class="form-control"
-                                        onChange={e =>{
-                                            this.setState({
-                                                elector:{
-                                                    ...this.state.elector,
-                                                    genero: e.target.value
-                                                }
-                                            })
-                                        }}
-                                    >
-                                    <option value="">Elegir</option>
-                                    <option value="femenino">Femenino</option>
-                                    <option value="masculino">Masculino</option>
-                                    </select>
                                 </div>
                             
-                                <div class="form-group col-md-6">
-                                    <label>Email</label>
-                                    <input
-                                     type="email"
-                                     class="form-control"
-                                     placeholder="Email"
-                                     onChange={e =>{
-                                        this.setState({
-                                            elector:{
-                                                ...this.state.elector,
-                                                email: e.target.value
-                                            }
-                                        })
-                                    }}
-                                     />
-                                </div>
-                            </div>
-
-                                                      
-                            <button type="submit" className="btn btn-primary float-right">Guardar Cambios</button>
-                    </form>
-
-                    )}
+                                <button type="submit" className="btn btn-primary float-right">Guardar Cambios</button>
+                        </form>
+                        )}
                     </Mutation>
                     </div> 
             </Fragment>
