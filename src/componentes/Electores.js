5df0d2a60ab192bea9586ee730bfe428218cf159
javier <jjzoto@gmail.com>
2019-04-25 14:28:05 -0500

    aspira  paginadorI

--- src/componentes/Electores.js
+++ src/componentes/Electores.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
 import { Query, Mutation } from 'react-apollo';
 import{Link} from 'react-router-dom';
 
@@ -6,60 +6,71 @@ import{Link} from 'react-router-dom';
 import { ELECTORES_QUERY } from '../querys';
 import { ELIMINAR_ELECTOR } from '../mutations';
 
-const Contactos = () => (
-    <Query query={ELECTORES_QUERY} pollInterval={500}>
-        {({ loading, error, data, startPolling, stopPolling }) => {
-            if(loading) return "cargando...";
-            if (error) return `Error: ${error.message}`;
-            console.log(data.getElectores);
+class Electores extends Component{
+    state = {
+        paginador:{
+            offset:0,
+            actual:1
+        }
+    }
 
-            
-        return(
-            <Fragment>
-                <h2 className="text-center">Listado de Electores</h2>
-                <ul className="list-group mt-4">
-                    {data.getElectores.map(item =>{
-                        const {id} = item;
-                        return(
-                        <li key={item.id} className="list-group-item">
-                            <div className="row justify-content-between aling-items-center">
-                                <div className="col-md-8 d-flex justify-content-between aling-items-center" >
-                                    {item.nombre} {item.apellido} 
+    render(){
+
+     return(
+        <Query query={ELECTORES_QUERY} pollInterval={500}>
+            {({ loading, error, data, startPolling, stopPolling }) => {
+                if(loading) return "cargando...";
+                if (error) return `Error: ${error.message}`;
+                console.log(data.getElectores);
+
+                
+            return(
+                <Fragment>
+                    <h2 className="text-center">Listado de Electores</h2>
+                    <ul className="list-group mt-4">
+                        {data.getElectores.map(item =>{
+                            const {id} = item;
+                            return(
+                            <li key={item.id} className="list-group-item">
+                                <div className="row justify-content-between aling-items-center">
+                                    <div className="col-md-8 d-flex justify-content-between aling-items-center" >
+                                        {item.nombre} {item.apellido} 
+                                    </div>
+                                    <div className="col-md-4 d-flex justify-content-end">
+                                    <Mutation mutation={ELIMINAR_ELECTOR}>
+                                        {eliminarElector => (
+                                            <button type="button" className="btn btn-danger d-block d-mb-inline-block mr-2"
+                                                onClick={ () => {
+
+                                                if(window.confirm('Seguro que Deseas Eliminar este Elector?')){
+                                                    eliminarElector({
+                                                        variables: {id}
+                                                    })
+                                                }
+                                                }}
+                                                >
+                                                &times; Eliminar
+                                            </button>
+                                        )}
+                                    </Mutation>
+                                        <Link to={`/elector/editar/${item.id}`} className="btn btn-success d-block d-med-inline-block"> Editar Elector</Link>
+                                    
+                                    </div>
+
                                 </div>
-                                <div className="col-md-4 d-flex justify-content-end">
-                                <Mutation mutation={ELIMINAR_ELECTOR}>
-                                    {eliminarElector => (
-                                         <button type="button" className="btn btn-danger d-block d-mb-inline-block mr-2"
-                                            onClick={ () => {
-
-                                            if(window.confirm('Seguro que Deseas Eliminar este Elector?')){
-                                                eliminarElector({
-                                                    variables: {id}
-                                                })
-                                            }
-                                            }}
-                                            >
-                                            &times; Eliminar
-                                        </button>
-                                    )}
-                                </Mutation>
-                                    <Link to={`/elector/editar/${item.id}`} className="btn btn-success d-block d-med-inline-block"> Editar Elector</Link>
-                                
-                                </div>
-
-                            </div>
-                        
-                        </li>
-                        )
-                    })}
-                </ul>
-            </Fragment>
-            
-        )
-        }}
+                            
+                            </li>
+                            )
+                        })}
+                    </ul>
+                </Fragment>
+                
+            )
+            }}
 
 
-    </Query>
-)
+        </Query>
+     )
+}
 
-export default Contactos;
+export default Electores;
