938e5c14c6cbbdbc2a21e6f7f3a3fe9cfc15ac07
javier <jjzoto@gmail.com>
2019-04-30 09:20:08 -0500

    anteror y siguiente

--- src/componentes/Paginador.js
+++ src/componentes/Paginador.js
@@ -2,10 +2,12 @@ import React, {Component} from 'react';
 
 class Paginador extends Component {
     state = { 
+
         paginador : {
             paginas: Math.ceil(Number(this.props.totalElectores) / this.props.limite)
         }
      }
+     
     render() {
         const {actual} = this.props;
         const btnAnterior = (actual > 1) ? <button type="button" className="btn btn-success mr-2">&laquo; Anterior</button>:'';  
